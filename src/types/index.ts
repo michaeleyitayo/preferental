413232fedf4a8328ef76b21e93177133bd5c2f78
michaeleyitayo <archangelmichael516@gmail.com>
2022-04-11 18:18:50 +0100

    fix: some type issues

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import { IReqUser, IUser } from "./user";
 
 interface CustomRequest extends Request {
-  user: IReqUser;
+  user?: IUser;
 }
 
 type IControllerFn = (req: Request, res: Response, next: NextFunction) => any;
