32a11f899c24b6e44920cea7810d8d590c37c8c9
michaeleyitayo <archangelmichael516@gmail.com>
2022-04-11 21:23:40 +0100

    user upgrade to agent

--- src/services/user.ts
+++ src/services/user.ts
@@ -3,3 +3,7 @@ import User from "../models/user";
 export const getAllUsers = async () => {
   return await User.find({});
 };
+
+export const upgradeUserToAgent = async (userId: string) => {
+  return await User.findByIdAndUpdate(userId, { $set: { role: "agent" } });
+};
