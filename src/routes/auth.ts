954265841c572d3243a2c2ac38771545c2830a63
michaeleyitayo <archangelmichael516@gmail.com>
2022-04-11 13:16:20 +0100

    auth sign up route

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,8 +1,7 @@
 import express from "express";
-import { loginController, signupController } from "../controllers/auth";
+import { signupController } from "../controllers/auth";
 const authRouter = express.Router();
 
-authRouter.post("/login", loginController);
 authRouter.post("/signup", signupController);
 
 export default authRouter;
